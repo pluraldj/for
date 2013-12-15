d28c5003bacbebc6645c7da106c17bf5ab9b3eb9
Lasse Carstensen <lasse.car@gmail.com>
2013-12-15 08:55:01 +0100

    add license blurb

--- src/Character.h
+++ src/Character.h
@@ -5,6 +5,21 @@
 //  Created by Lasse Carstensen on 29/11/13.
 //  Copyright (c) 2013 Lasse Carstensen. All rights reserved.
 //
+// This file is part of FOR.
+//
+// FOR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// FOR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with FOR (see file LICENSE).  If not, see <http://www.gnu.org/licenses/>.
+
 
 #ifndef __forogue__Character__
 #define __forogue__Character__
@@ -52,6 +67,7 @@ public:
     
     Visibility *visInfo;
     
+    // STATS
     int level;
     int XP;
     
@@ -100,6 +116,11 @@ public:
     
     string name;
     string title;
+    
+    // Kill counter
+    // Type of creature killed and number of kills
+    // Should sort by number of kills (max first)
+    vector< pair<EntityType,int> > killsList;
     
 private:
 };
