43b460f278116fb3cb21db35ec6418b1703b7945
Lasse Carstensen <lasse.car@gmail.com>
2013-12-30 10:12:28 +0100

    A* pathfinding

--- src/AStarSearcher.cpp
+++ src/AStarSearcher.cpp
@@ -22,10 +22,46 @@
 
 #include "AStarSearcher.h"
 
-AStarSearcher::AStarSearcher()
+AStarSearcher::AStarSearcher(Location *_loc) : m_grid(create_grid(_loc->size.x,_loc->size.y)), m_barrier_grid(create_barrier_grid(_loc))
 {
+    location = _loc;
 }
 
 AStarSearcher::~AStarSearcher()
 {
+}
+
+// Initialise grid structure based on dimensions of tilemap
+grid AStarSearcher::create_grid(size_t x, size_t y)
+{
+    boost::array<size_t, 2> lengths = { {x, y} };
+    return grid(lengths);
+}
+
+filtered_grid AStarSearcher::create_barrier_grid(Location *loc)
+{
+    // Iterate over all tiles, add inaccessible to barrier vertex set
+    int sx = loc->size.x;
+    int sy = loc->size.y;
+    for ( int x=0; x<sx; ++x)
+    {
+        for ( int y=0; y<sy; ++y )
+        {
+            Tile *tile = loc->GetTile(x,y);
+            
+            if ( tile ->clipMask == false )
+            {
+                // Convert to vertex index
+                int index = y*sy+sx;
+                
+                // get vertex descriptor
+                vertex_descriptor u = vertex(index,m_grid);
+                
+                // Add to barrier vertex set
+                m_barriers.insert(u);
+            }
+        }
+    }
+
+    return boost::make_vertex_subset_complement_filter(m_grid, m_barriers);
 }
--- src/AStarSearcher.h
+++ src/AStarSearcher.h
@@ -40,7 +40,14 @@
 #include <ctime>
 #include <iostream>
 
+#include "Location.h"
+
 using namespace std;
+
+// The domain of the distance function is _integers_, that is the number of steps we need to walk to get somewhere.
+// This is different from using Euclidean distance (direct line)
+// This shouldn't really make much difference
+typedef unsigned int dist;
 
 // typedef Boost types for this particular problem
 typedef boost::grid_graph<2> grid;
@@ -57,6 +64,14 @@ struct vertex_hash:unary_function<vertex_descriptor, size_t> {
     }
 };
 
+// Specific data types we use
+typedef boost::unordered_set<vertex_descriptor, vertex_hash> vertex_set;
+typedef boost::vertex_subset_complement_filter<grid, vertex_set>::type
+filtered_grid;
+typedef boost::unordered_map<vertex_descriptor,vertex_descriptor,vertex_hash> pred_map;
+typedef boost::unordered_map<vertex_descriptor,dist,vertex_hash> dist_map;
+
+
 // Abstracted representation of a graph to search and methods to do it using A*
 // For example, dungeons construct an instance of this for pathfinding
 class AStarSearcher
@@ -64,10 +79,80 @@ class AStarSearcher
 public:
     // Make from world or object
     // TODO
-    AStarSearcher();
+    AStarSearcher(Location *_loc);
     ~AStarSearcher();
     
+    // TODO
+    vertex_descriptor source() const { return vertex(0, m_grid);}
+    vertex_descriptor goal() const { return vertex(num_vertices(m_grid)-1, m_grid);}
+    
 private:
+    // Create the underlying rank-2 grid with the specified dimensions.
+    grid create_grid(size_t x, size_t y);
+
+    // Filter the barrier vertices out of the underlying grid.
+    filtered_grid create_barrier_grid(Location *loc);
+    
+    bool solve();
+    bool solved();  // solution found?
+    
+    // Is vertex part of shortest path?
+    bool solution_contains(vertex_descriptor u) const
+    {
+        return m_solution.find(u) != m_solution.end();
+    }
+    
+    // The grid underlying the maze
+    grid m_grid;
+    // The underlying maze grid with barrier vertices filtered out
+    filtered_grid m_barrier_grid;
+    // The barriers in the maze
+    vertex_set m_barriers;
+    // The vertices on a solution path through the maze
+    vertex_set m_solution;
+    // The length of the solution path
+    dist m_solution_length;
+    
+    // Tile-containing object we are building search graph for
+    Location *location;
+};
+
+// Heuristic for the distance function
+// This calculates the Manhattan distance (taxicab metric) between a vertex and a goal
+// vertex.
+class heuristic:
+public boost::astar_heuristic<filtered_grid, dist>
+{
+public:
+    heuristic(vertex_descriptor goal):m_goal(goal) {};
+    
+    dist operator()(vertex_descriptor v)
+    {
+        return abs((int)(m_goal[0] - v[0])) + abs((int)(m_goal[1] - v[1]));
+    }
+    
+private:
+    vertex_descriptor m_goal;
+};
+
+// Something for Boost to throw when a solution is found
+// It doesn't need any structure
+struct found_goal {};
+
+// Visitor class for travering nodes
+// Throw found_goal to signal goal found for termination
+struct astar_goal_visitor : public boost::default_astar_visitor
+{
+    astar_goal_visitor(vertex_descriptor goal) : m_goal(goal) {};
+    
+    void examine_vertex(vertex_descriptor u, const filtered_grid&)
+    {
+        if (u == m_goal)
+            throw found_goal();
+    }
+    
+private:
+    vertex_descriptor m_goal;
 };
 
 #endif /* defined(__forogue__AStarSearcher__) */
